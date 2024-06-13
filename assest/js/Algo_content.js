4b7d482633a59b2a8c429f52197283fdb8c87466
deep-maurya <deepakmauryahd@gmail.com>
2024-06-13 10:53:57 +0530

    Added : Binary search & Queue Visualizer

--- assest/js/Algo_content.js
+++ assest/js/Algo_content.js
@@ -7,6 +7,30 @@ const data = [
     "usage": "As per your DSA/Algorithm",
     "time complexity": "As per your DSA/Algorithm",
     "Space complexity": "As per your DSA/Algorithm"
+  },
+  {
+    "content_id": 2183,
+    "title": "Queue",
+    "overview": `A Queue Data Structure is a fundamental concept in computer science used for storing and managing data in a specific order. It follows the principle of “First in, First out” (FIFO), where the first element added to the queue is the first one to be removed. Queues are commonly used in various algorithms and applications for their simplicity and efficiency in managing data flow.
+        <img style="width:100%" class="img-fluid mt-2" src ='./assest/image/queue.png'>`,
+    "What is Queue in Data Structures?": "A queue is a linear data structure that follows the First-In-First-Out (FIFO) principle. It operates like a line where elements are added at one end (rear) and removed from the other end (front).",
+    "Basic Operations of Queue Data Structure?": `<b>Enqueue (Insert):</b> Adds an element to the rear of the queue.<br>
+        <b>Dequeue (Delete):</b> Removes and returns the element from the front of the queue.<br>
+        <b>Peek:</b> Returns the element at the front of the queue without removing it.<br>
+        <b>Empty:</b> Checks if the queue is empty.<br>
+        <b>Size:</b> Checks number of elements in it.`,
+    "Applications of Queue": `Task scheduling in operating systems
+        Data transfer in network communication
+        Simulation of real-world systems (e.g., waiting lines)
+        Priority queues for event processing queues for event processing`,
+  },
+  {
+    "content_id": 3183,
+    "title": "Binary Search",
+    "Binary Search": `Binary Search is a searching algorithm for finding an element's position in a sorted array. In this approach, the element is always searched in the middle of a portion of an array. <br><i>Binary search can be implemented only on a sorted list of items. If the elements are not sorted already, we need to sort them first.</i>`,
+    "Time Complexities": `<b>Best case complexity </b>: O(1)<br>
+        <b>Average case complexity </b>: O(log n)<br>
+        <b>Worst case complexity </b>: O(log n)`
   }
 ];
 
--- assest/js/algo_list.js
+++ assest/js/algo_list.js
@@ -5,5 +5,19 @@ const algorithms_Data = [
         "text": "Basic template for Algorithms",
         "redirect_page": "template.html",
         "content_id" : 1
+    },
+    {
+        "id": "2183",
+        "label": "Queue",
+        "text": "Operates on a First In, First Out (FIFO) principle",
+        "redirect_page": "queue.html",
+        "content_id" : 2183
+    },
+    {
+        "id": "3183",
+        "label": "Binary Search",
+        "text": "Repeatedly dividing the search interval in half.",
+        "redirect_page": "binary_search.html",
+        "content_id" : 3183
     }
 ];
